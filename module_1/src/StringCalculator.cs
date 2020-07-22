c7e7a4165a7ad87b54d089f44f38835ea3ee3f63
doomin <dominik.pszczola@gmail.com>
2020-07-22 20:58:26 +0200

    Refactored project set up

---
+++ module_1/src/StringCalculator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace test_project
+{
+    class StringCalculator
+    {
+        public int Add(String input){
+            if(String.IsNullOrEmpty(input)){
+                return 0;
+            }
+
+            String[] numbers = input.Split(",");
+            if(numbers.Length == 1){
+                String number = numbers[0];
+                return GetIntFrom(number);
+            }else{
+                return GetIntFrom(numbers[0]) + GetIntFrom(numbers[1]); 
+            }
+        }
+
+        private int GetIntFrom(String number) {return int.Parse(number);}
+    }
+}
---
+++ module_1/test/Tests.cs
@@ -0,0 +1,39 @@
+using NUnit.Framework;
+using System;
+
+namespace test_project{
+
+    [TestFixture]
+    public class Module1{
+        
+        private StringCalculator calculator;
+
+        [SetUp]
+        public void Setup(){
+            calculator = new StringCalculator();
+        }
+
+        [Test]
+        public void ShouldReturnANumberWhenNumberGiven(){
+            Assert.AreEqual(2, calculator.Add("2"));
+        }
+        
+        [Test]
+        public void ShouldReturnSumOfTwoNumbersWhenTwoNumbersGiven(){
+            Assert.AreEqual(3, calculator.Add("2,1"));
+        }
+
+        [TestCase("1,2", 3)]
+        [TestCase("1,2,3", 6)]
+        [TestCase("1,2,3,4", 10)]
+        [TestCase("2,5,6,8,9,7,5,2,1,9", 54)]
+        [TestCase("-1, 1", 0)]
+        [TestCase("-2, -1", -3)]
+        [TestCase("0", 0)]
+        public void ShoudReturnSumOfNumbersWhenACommaSeparatedListOfNumbersGiven(string input, int expected)
+        {
+            Assert.AreEqual(expected, calculator.Add(input));
+        }   
+    }   
+}
+
