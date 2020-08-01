228414f9da99e2fa02feefe9406af8e1afdb7fc5
doomin <dominik.pszczola@gmail.com>
2020-08-01 22:14:30 +0200

    Module 3 - Bowling Game kata

---
+++ module_3/src/BowlingGame.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+
+namespace szkola_testow.module_3.src
+{
+    class BowlingGame
+    {
+        private int score;
+
+        public int GetScore()
+        {
+            return score;
+        }
+
+        public int Roll(int pins)
+        {
+            score += pins;
+            return score;
+        }
+    }
+}
---
+++ module_3/test/BowlingGameTests.cs
@@ -0,0 +1,35 @@
+﻿using FluentAssertions;
+using szkola_testow.module_3.src;
+using NUnit.Framework;
+
+namespace szkola_testow.module_3.test
+{
+    class BowlingGameTests
+    {
+        BowlingGame game;
+
+        [SetUp]
+        public void NewGame()
+        {
+            game = new BowlingGame();
+        }
+
+        [Category("module3")]
+        [Test]
+        public void ShouldReturnZeroWhenNoRollsDone()
+        {           
+            game.GetScore().Should().Be(0);
+        }
+
+        [Category("module3")]
+        [Test]
+        public void ShouldReturn20When20PinsDown()
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                game.Roll(1);
+            }
+            game.GetScore().Should().Be(20);
+        }
+    }
+}
