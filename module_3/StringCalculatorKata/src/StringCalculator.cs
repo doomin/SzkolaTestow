4ca79029b36583ac9764cf4e59f14e2fa0526028
doomin <dominik.pszczola@gmail.com>
2020-08-02 18:53:17 +0200

    String Calculator Kata functionality added based on tests

---
+++ module_3/StringCalculatorKata/src/StringCalculator.cs
@@ -0,0 +1,37 @@
+﻿using Castle.Core.Internal;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace szkola_testow.module_3.StringCalculatorKata.src
+{
+    class StringCalculator
+    {
+        private int result;
+
+        public int Add(string input)
+        {
+            if (input.IsNullOrEmpty())
+            {
+                return 0;
+            }
+            
+            foreach (string number in SplitStringInput(input))
+            {
+                result += ParseStringToInt(number);
+            }
+            return result;
+        }
+
+        private static int ParseStringToInt(string number)
+        {
+            return int.Parse(number);
+        }
+
+        private string[] SplitStringInput(string input)
+        {
+            char[] delims = new[] {',', '\n' };
+            return input.Split(delims);
+        }
+    }
+}
--- module_3/StringCalculatorKata/test/StringCalculatorTests.cs
+++ module_3/StringCalculatorKata/test/StringCalculatorTests.cs
@@ -1,17 +1,46 @@
-﻿using NUnit.Framework;
-using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using FluentAssertions;
+using NUnit.Framework;
+using System.Security.Cryptography;
 
 namespace szkola_testow.module_3.StringCalculatorKata.test
 {
     class StringCalculatorTests
     {
+        private src.StringCalculator stringCalculator;
 
-        [Test]
-        public void ShouldReturnZeroWhenPaaingEmptyString()
+        [SetUp]
+        public void CreateCalculatorInstance()
         {
+            stringCalculator = new src.StringCalculator();
+        }
 
+        [Category("StringCalculatorKata")]
+        [Test]
+        public void ShouldReturnZeroWhenPasingEmptyString()
+        {
+            stringCalculator.Add("").Should().Be(0);
+        }
+
+        [Category("StringCalculatorKata")]
+        [Test]
+        public void ShouldReturnOneWhenPasingNumberOne()
+        {
+            stringCalculator.Add("1").Should().Be(1);
+        }
+
+        [Category("StringCalculatorKata")]
+        [TestCase("1,2", 3)]
+        [TestCase("1,2,3,4,5,6,7,8,9,0", 45)]
+        public void ShouldReturnSumOfNumbersWhenPasingNumbersSeparatedByComma(string input, int result)
+        {
+            stringCalculator.Add(input).Should().Be(result);
+        }
+
+        [Category("StringCalculatorKata")]
+        [Test]
+        public void ShouldHandleNewLinCharInInputString()
+        {
+            stringCalculator.Add("1\n2,3").Should().Be(6);
         }
     }
 }
