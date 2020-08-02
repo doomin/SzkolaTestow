f2e495c318f16ab8423d1add8b505b5311e80a8e
doomin <dominik.pszczola@gmail.com>
2020-08-02 18:06:40 +0200

    String Calculator Kata init

---
+++ module_3/StringCalculatorKata/test/StringCalculatorTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace szkola_testow.module_3.StringCalculatorKata.test
+{
+    class StringCalculatorTests
+    {
+
+        [Test]
+        public void ShouldReturnZeroWhenPaaingEmptyString()
+        {
+
+        }
+    }
+}
